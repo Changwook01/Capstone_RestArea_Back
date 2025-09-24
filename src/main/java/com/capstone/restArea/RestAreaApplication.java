d568db37d6061f96de5bac909685bd47fdd7c3b3
Changwook01 <tjckddnr3026@naver.com>
2025-09-24 11:05:19 +0900

    Feat: Spring Boot 프로젝트 생성

---
+++ src/main/java/com/capstone/restArea/RestAreaApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.restArea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestAreaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/restArea/RestAreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.restArea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAreaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
